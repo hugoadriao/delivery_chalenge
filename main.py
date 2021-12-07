1abaf8127e25aed3167e317fe2390e7c4610dda7
Hugo Victor dos Santos Silva <hugovictorsilva94@gmail.com>
2021-12-07 00:41:39 -0300

    update api metadata

--- main.py
+++ main.py
@@ -10,11 +10,46 @@ import db
 from utils import (shuffle_orders, bike_accept_request,
                    assign_priority_order, build_response)
 
-app = FastAPI()
+description = """
+## **This project is focused in solve a specific problem and has two scenario:**
+### Scenarios:
+* The more simple where the user only want to get pre-existing data from db. This case use the routes ```/bike/{bike_id}``` and ```/bikes```
+* The more complex consider a scenario where the bikes has the option to accept or not the request that randomly arrive. This case use ```order_assign``` and it randomly generates the order of requests and solve the problem every time the route is called.
+"""
+
+tags_metadata = [
+    {
+        'name': 'complex-scenario',
+        'description': 'Simulates a simple "real world" to a specific scenario'
+    },
+    {
+        'name': 'get-bike-id',
+        'description': 'Get a specific bike from a pre-existing database where the problem is solved'
+    },
+    {
+        'name': 'get-all-bikes',
+        'description': 'Get all bikes from a pre-existing database where the problem is solved'
+    }
+]
+
+app = FastAPI(
+    title="Delivery Challenge",
+    description=description,
+    version="0.0.10",
+    openapi_tags=tags_metadata,
+    contact={
+        'name': 'You can find the source code here',
+        'url': 'https://github.com/hugoadriao/delivery_chalenge'
+    }
+)
 
 
-@app.get("/api/v1/order_assign/")
+@app.get("/api/v1/order_assign/", tags=['complex-scenario'])
 def order_assign():
+    """
+    This end-point simulates a simple world where the request arrive in
+    a random order and the bikes chose to accept or not some request
+    """
     stores = copy.deepcopy(db.stores.get('stores'))
     bikes = copy.deepcopy(db.bikes_id)
 
@@ -28,15 +63,22 @@ def order_assign():
     return build_response(bikes)
 
 
-@app.get("/api/v1/bike/{bike_id}")
-def get_bike(bike_id):
+@app.get("/api/v1/bike/{bike_id}", tags=['get-bike-id'])
+def get_bike(bike_id: int):
+    """
+    This end-point return a specific bike by it's ID.
+    bike_id -> int (1...5)
+    """
     with open('result.json') as data_base:
         data = json.load(data_base)
 
     return {f'Moto {bike_id}': data.get(f'Moto {bike_id}')}
 
 
-@app.get("/api/v1/bikes/")
+@app.get("/api/v1/bikes/", tags=['get-all-bikes'])
 def get_bikes():
+    """
+    This end-point return all bikes from database
+    """
     with open('result.json') as data_base:
         return json.load(data_base)
